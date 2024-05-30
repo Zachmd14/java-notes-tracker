d35db94d2c9f9449110d9484b6fa4cf1e9d49e1c
zach <zacharymd14@gmail.com>
2024-05-30 22:14:30 +0200

    basically a (bugged) todolist for now

--- app/App.java
+++ app/App.java
@@ -1,12 +1,49 @@
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
 
+		// Initialize the 'Notes' class
 		Notes myNote = new Notes();
+
+		// Initialize the Scanner method
+		Scanner scan = new Scanner(System.in);
 
 		System.out.println("\n===== Grades Tracker App =====");
 
-		myNote.createNote();
-		myNote.seeNotes();
+		while (true) {
+			System.out.println("\nSelect one of the following option");
+			System.out.println("1. Add a new note");
+			System.out.println("2. Delete a note");
+			System.out.println("3. See all existing notes");
+			System.out.println("4. Quit");
+
+			// Ask user the menu he want to go in
+			System.out.print("\nGo menu : ");
+			int menuInput = scan.nextInt();
+
+			switch (menuInput) {
+				case 1:
+					myNote.createNote();
+					break;
+
+				case 2:
+					myNote.deleteNote();
+					break;
+
+				case 3:
+					myNote.seeNotes();
+					break;
+
+				case 4:
+					System.out.println("Exiting...");
+					System.exit(0);
+					break;
+
+				default:
+					break;
+			}
+		}
 
 	}
 }
--- app/Notes.java
+++ app/Notes.java
@@ -14,20 +14,43 @@ public class Notes {
 		// No initialization needed here for now
 	}
 
-	/**
-	 * METHOD
-	 * Create a new note
-	 */
 	public void createNote() {
-		System.out.print("Enter your note title: ");
+		System.out.println("\n===== New note =====");
+		System.out.print("\nEnter your note title: ");
 		Notes.noteTitle = Notes.scan.nextLine();
-		Notes.titleArray.add(Notes.noteTitle); // Add the note title to the titleArray
-		System.out.println("Title: " + Notes.noteTitle);
+		Notes.titleArray.add(Notes.noteTitle);
+	}
+
+	public void deleteNote() {
+		System.out.println("\n===== Delete note =====");
+
+		System.out.println("Do you want to see all of the existings note ? (y/n) : ");
+		String userValidation = scan.nextLine();
+		if (userValidation == "y") {
+			this.seeNotes();
+			System.out.print("\nEnter the number of the note you want to delete : ");
+			int noteToDeleteNumber = scan.nextInt();
+			if (titleArray.contains(noteToDeleteNumber)) {
+				Notes.titleArray.remove(noteToDeleteNumber);
+			} else {
+				System.out.println("There is no note having this number");
+			}
+
+		} else {
+			System.out.print("\nEnter the number of the note you want to delete : ");
+			int noteToDeleteNumber = scan.nextInt();
+			if (titleArray.contains(noteToDeleteNumber)) {
+				Notes.titleArray.remove(noteToDeleteNumber);
+			} else {
+				System.out.println("There is no note having this number");
+			}
+		}
 	}
 
 	public void seeNotes() {
+		System.out.println("\n===== Note list ===== ");
 		for (int i = 0; i < Notes.titleArray.size(); i++) {
-			System.out.println(Notes.titleArray.get(i));
+			System.out.println((i + 1) + " : " + Notes.titleArray.get(i));
 		}
 	}
 }
